a979260ce9e6a1c0a17d198d1073d58784fe8c9c
AlexGMCA <stu22197@ashs.school.nz>
2022-08-27 11:35:07 +1200

    Better cooking

--- My project (3)/Assets/Scripts/BeefColourChange.cs
+++ My project (3)/Assets/Scripts/BeefColourChange.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class BeefColourChange : MonoBehaviour
 {
-    public GlobalVariables GlobalVariables;
+    //Calling the different stages of beef, global variables and making the two variables use in this script.
+    public GlobalVariables globalVariables;
     public Sprite Beef1;
     public Sprite Beef2;
     public Sprite Beef3;
     public int Time = 0;
+    public bool flip = false;
+    public GameObject Arrow;
+    public GameObject Text;
+    public bool Flipped = false;
+
+    public void OnMouseDown()
+    {
+        if(flip == true)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Beef1;
+            flip = false;
+            Time = 0;
+            Flipped = true;
+        }
+    }
 
     void Update()
     {
-        if(GlobalVariables.Cooking == true)
+        if(Time > 1000 && Time < 1500 && Flipped == false)
+        {
+            flip = true;
+        }
+        //Activating the button to flip the steak.
+        if(flip == true)
+        {
+            Arrow.SetActive(true);
+            Text.SetActive(true);
+        }
+        else{
+            Arrow.SetActive(false);
+            Text.SetActive(false);   
+            }
+        //Changes the stage of the beef over time.
+        if(globalVariables.Cooking == true && flip == false)
         {
             Time = Time + 1;
         }
@@ -30,8 +61,8 @@ public class BeefColourChange : MonoBehaviour
         if(Time > 1500 && Time < 2000)
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = Beef3;
-            GlobalVariables.Cooked = true;
-            GlobalVariables.Cooking = false;
+            globalVariables.Cooked = true;
+            globalVariables.Cooking = false;
         }
     }
 }
