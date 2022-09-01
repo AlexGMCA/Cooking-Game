32770db1a54fd273f2037a22f4e388fdf070a173
AlexGMCA <stu22197@ashs.school.nz>
2022-09-02 10:28:37 +1200

    Bug Fix V3

--- My project (3)/Assets/Scripts/Finishedcooking.cs
+++ My project (3)/Assets/Scripts/Finishedcooking.cs
@@ -10,15 +10,18 @@ public class Finishedcooking : MonoBehaviour
     public GlobalVariables GlobalVariables;
     public GameObject Pan;
     public Sprite CookPan;
+    public Transform BPan;
+    public Transform PanCords;
 
     void Update()
     {
-        //Telling the game that the beef is  cooked and to change scenes.
+        //Telling the game that the beef is  cooked and to change scenes and change the pans cordinates so that the game does not break.
         if(GlobalVariables.Cooked == true && GlobalVariables.AtBoard == false)
         {
             Parent.SetActive(true);
             StoveTop.SetActive(false);
             Pan.GetComponent<SpriteRenderer>().sprite = CookPan; 
+            BPan.position = PanCords.position;
         }
     }
 }
